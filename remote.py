e31676cda407556efc3afcf421353d83dcf3cd2d
mokkko <mokkko@github.com>
2020-08-13 19:39:53 +0200

    cleaning up and adding some comments

--- remote.py
+++ remote.py
@@ -3,11 +3,11 @@
 # the user may choose what actions should take place.
 # the camera control code will be written to ommand.txt.
 # There may also be the functionality of controlling camera movement - this code might be sent to motion.txt. 
+
 #
-# brainstorming 
+# brainstorming of possible functionality
 #
-#
-#
+
 # 1 - Start surveillance program
 # 2 - Stop surveillance program
 # 3 - Record video and send it to email address
@@ -16,24 +16,15 @@
 # 6 - ...
 # 7 - ...
 # 8 - ...
-# 9 - ...
+# 9 - Status (e.g. recording mode)
 # 0 - Enter code manually (hit ENTER after typing code)
 # q - Quit remote program
 #
-# according to keystroke: generate file "command.txt" and write the code into it.
-#
-#
-#
-# menu key 1 pressed
-#
-#
-# echo "key 1 was pressed." >> ~/pi/APP__surveillance/testfile.txt
-#
-#
+# according to keystroke / input: generate file "command.txt" and write the code into it.
 #
 
 def menu():
-	print("\nMake your choice, master: \n 1 - Start program \n 2 - Stop program \n 3 - Write text to testfile.txt \n")
+	print("\nMake your choice: \n 1 - Start program \n 2 - Stop program \n 3 - Write text to testfile.txt \n")
 	choice = input()
 
 	if choice == "1":
@@ -44,6 +35,12 @@ def menu():
 		print("Program stopped!")
 		menu()
 
+# At the moment, choices 1 and 2 do not seem to work: no text is printed on the screen.
+
 	if choice == "3":
 		echo "this line was created via remote!" >> ./testfile.txt
+		
+# At the moment the code above this line causes a syntax error. When typed directly in the terminal, it works!
+# (It then creates a text line in the testfile.)
+		
 menu()
