25d0391e67c7cdcf72e455b3d4674f993675436b
mokkko <mokkko@github.com>
2020-08-14 19:22:39 +0200

    added some comments

--- remote.py
+++ remote.py
@@ -18,7 +18,7 @@ import io
 # 6 - ...
 # 7 - ...
 # 8 - ...
-# 9 - Status (e.g. recording mode)
+# 9 - Status (e.g. recording mode, temperature, cpu and mem data)
 # 0 - Enter code manually (hit ENTER after typing code)
 # q - Quit remote program
 #
@@ -26,7 +26,7 @@ import io
 #
 
 def menu():
-	print("\nMake your choice: \n 1 - Start program \n 2 - Stop program \n 3 - Write text to command.txt \n")
+	print("\nMake your choice: \n 1 - Start program \n 2 - Stop program \n 3 - Quit surveillance \n")
 	choice = str(input())
 
 	if choice == "1":
@@ -37,12 +37,12 @@ def menu():
 		print("Program stopped!")
 		menu()
 
-
 	if choice == "3":
-		f = io.open("./command.txt","w")
-		f.write(u'This line was written from remote.py\n')
-		
-# At the moment the code above this line causes a syntax error. When typed directly in the terminal, it works!
-# (It then creates a text line in the testfile.)
-		
+		f = io.open("./command.txt","w")	# open command.txt file
+		f.write(u'quit')			# write code to command.txt
+
+#
+# is it recommended to close the command.txt file after writing?
+#
+
 menu()
